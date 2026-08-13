045c13a39f89893dc53ce854e69e55461d213c6c
LttlMario <bmariomihail@gmail.com>
2026-08-13 11:49:39 +0300

    Update Panel Pro release version

--- js/project-version.js
+++ js/project-version.js
@@ -2,8 +2,8 @@
   "use strict";
 
   window.PANEL_RELEASE = Object.freeze({
-    version: "3.5.0",
-    releasedAt: "2026-08-05",
+    version: "3.6.0",
+    releasedAt: "2026-08-13",
     changelogUrl: "changelog.html",
     developerUrl: "developer.html"
   });
