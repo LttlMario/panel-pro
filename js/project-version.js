dddff97e264c50d137eb1f843567be3006ad250e
lttlmario <you@example.com>
2026-08-20 21:20:12 +0300

    fix: widen diagnostic and logs content only

--- js/project-version.js
+++ js/project-version.js
@@ -2,7 +2,7 @@
   "use strict";
 
   window.PANEL_RELEASE = Object.freeze({
-    version: "3.11.2",
+    version: "3.11.3",
     releasedAt: "2026-08-20",
     changelogUrl: "changelog.html",
     developerUrl: "developer.html"
